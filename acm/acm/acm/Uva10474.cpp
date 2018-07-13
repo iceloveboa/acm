b2ca3a6f480a1e034f2ac7102d996966166d880c
垣衣 <ice.pt@alibaba-inc.com>
2018-07-13 16:09:53 +0800

    add

--- acm/acm/acm/Uva10474.cpp
+++ acm/acm/acm/Uva10474.cpp
@@ -6,10 +6,31 @@
 //  Copyright © 2018 ice. All rights reserved.
 //
 
-#include <iostream>
+//#include <iostream>
+//using namespace std;
+
+#include<cstdio>
+#include <algorithm>
+
 using namespace std;
 
+const int maxn = 10000;
+
+
 int main(int argc, const char * argv[]){
-    cout<<"ice"<<endl;
-    return 0;
+    int n,q,x,a[maxn],kase = 0;
+    
+    while (scanf("%d%d",&n,&q) == 2 && n) {
+        printf("CASE# %d:\n",++kase);
+        for (int i=0; i<n; i++) {
+            scanf("%d",&a[i]);
+        }
+        sort(a, a+n);
+        while (q--) {
+            scanf("%d",&x);
+            int p = lower_bound(a,a+n,x) - a ;
+            if(a[p] == x) printf("%d found at %d\n",x,p+1);
+            else printf("%d not found\n",x);
+        }
+    }
 }
