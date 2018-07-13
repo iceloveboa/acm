cd977ba4cb561bab21e0c8a5f22548fcbcae095d
垣衣 <ice.pt@alibaba-inc.com>
2018-07-13 17:49:56 +0800

    add

--- acm/acm/acm/Uva10474.cpp
+++ acm/acm/acm/Uva10474.cpp
@@ -1,36 +1,36 @@
+////
+////  Uva10474.cpp
+////  acm
+////
+////  Created by ice on 2018/7/13.
+////  Copyright © 2018 ice. All rights reserved.
+////
 //
-//  Uva10474.cpp
-//  acm
+////#include <iostream>
+////using namespace std;
 //
-//  Created by ice on 2018/7/13.
-//  Copyright © 2018 ice. All rights reserved.
+//#include<cstdio>
+//#include <algorithm>
 //
-
-//#include <iostream>
 //using namespace std;
-
-#include<cstdio>
-#include <algorithm>
-
-using namespace std;
-
-const int maxn = 10000;
-
-
-int main(int argc, const char * argv[]){
-    int n,q,x,a[maxn],kase = 0;
-    
-    while (scanf("%d%d",&n,&q) == 2 && n) {
-        printf("CASE# %d:\n",++kase);
-        for (int i=0; i<n; i++) {
-            scanf("%d",&a[i]);
-        }
-        sort(a, a+n);
-        while (q--) {
-            scanf("%d",&x);
-            int p = lower_bound(a,a+n,x) - a ;
-            if(a[p] == x) printf("%d found at %d\n",x,p+1);
-            else printf("%d not found\n",x);
-        }
-    }
-}
+//
+//const int maxn = 10000;
+//
+//
+//int main(int argc, const char * argv[]){
+//    int n,q,x,a[maxn],kase = 0;
+//    
+//    while (scanf("%d%d",&n,&q) == 2 && n) {
+//        printf("CASE# %d:\n",++kase);
+//        for (int i=0; i<n; i++) {
+//            scanf("%d",&a[i]);
+//        }
+//        sort(a, a+n);
+//        while (q--) {
+//            scanf("%d",&x);
+//            int p = lower_bound(a,a+n,x) - a ;
+//            if(a[p] == x) printf("%d found at %d\n",x,p+1);
+//            else printf("%d not found\n",x);
+//        }
+//    }
+//}
---
+++ acm/acm/acm/Uva10815.cpp
@@ -0,0 +1,42 @@
+//
+//  Uva10815.cpp
+//  acm
+//
+//  Created by ice on 2018/7/13.
+//  Copyright © 2018 ice. All rights reserved.
+//
+
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    set<string> dict;
+    string s,buf;
+    while (cin>>s) {
+        for (int i = 0 ; i<s.length(); i++) {
+            if (isalpha(s[i])) {
+                s[i] = tolower(s[i]);
+            }else{
+                s[i] = ' ';
+            }
+        }
+        
+        stringstream ss(s);
+        while (ss>>buf) {
+            dict.insert(buf);
+        }
+    }
+    
+    for (set<string>::iterator it = dict.begin(); it != dict.end() ; it++) {
+        cout<<*it<<endl;
+    }
+    
+    return 0;
+}
+
