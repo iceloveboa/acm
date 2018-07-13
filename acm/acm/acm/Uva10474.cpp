171eadd27e80ccae77526375ddc881e0c5faba78
垣衣 <ice.pt@alibaba-inc.com>
2018-07-13 15:35:35 +0800

    add

---
+++ acm/acm/acm/Uva10474.cpp
@@ -0,0 +1,15 @@
+//
+//  Uva10474.cpp
+//  acm
+//
+//  Created by ice on 2018/7/13.
+//  Copyright © 2018 ice. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    cout<<"ice"<<endl;
+    return 0;
+}
--- acm/acm/main.cpp
+++ acm/acm/main.cpp
@@ -6,10 +6,10 @@
 //  Copyright © 2018 ice. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
